7ccb9dbec5a545e8f4d399caa4881a2c546ae46e
MAvi-Rup <avirupmondal19@gmail.comm>
2024-02-09 02:01:15 +0600

    working with login and Signup

---
+++ src/redux/features/user/userSlice.ts
@@ -0,0 +1,52 @@
+import { auth } from '@/lib/firebase';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+interface IUserState {
+  user: {
+    email: string;
+  };
+  isLoading: boolean;
+  isError: boolean;
+  error: string | null;
+}
+const initialState: IUserState = {
+  user: {
+    email: null,
+  },
+  isError: false,
+  isLoading: false,
+  error: null,
+};
+
+export const createUser = createAsyncThunk(
+  'user/createUser',
+  async (email, password) => {
+    const data = await createUserWithEmailAndPassword(auth, email, password);
+    return data.user.email;
+  }
+);
+
+const userSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(createUser.pending, (state) => {
+        state.isLoading = true;
+        state.isError = false;
+        state.error = null;
+      })
+      .addCase(createUser.fulfilled, (state, action) => {
+        state.user.email = action.payload;
+        state.isLoading = false;
+      })
+      .addCase(createUser.rejected, (state, action) => {
+        state.user.email = null;
+        state.isError = true;
+        state.error = action.error.message;
+      });
+  },
+});
+
+export default userSlice.reducer;
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,21 +1,20 @@
-import {configureStore} from "@reduxjs/toolkit"
+import { configureStore } from '@reduxjs/toolkit';
+import { api } from './api/apiSlice';
 import cartReducer from './features/cart/cartSlice';
 import productReducer from './features/products/productSlice';
-import {api} from './api/apiSlice'
-
+import userReducer from './features/user/userSlice';
 const store = configureStore({
-    reducer:{
-        cart: cartReducer,
-        product: productReducer,
-        [api.reducerPath]:api.reducer,
-    },
-    middleware: (getDefaultMiddleware) =>
+  reducer: {
+    cart: cartReducer,
+    product: productReducer,
+    user: userReducer,
+    [api.reducerPath]: api.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(api.middleware),
-
-})
+});
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
-
 
 export default store;
